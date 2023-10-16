550308040ead20b04a321f9bd3133590ddb0b6d5
SamSweet04 <s_arystanbek@kbtu.kz>
2023-10-17 01:03:39 +0600

    added map, contact form and header info

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,7 +1,3 @@
-<<<<<<< HEAD
-=======
-
->>>>>>> origin/master
 import React, { useState } from 'react';
 import './Login.css';
 
@@ -20,7 +16,6 @@ function Login() {
                 <div className="text">Sign In</div>
                 <div className="underline"></div>
             </div>
-<<<<<<< HEAD
             <form className="inputs">
                 <div className="input">
                     <img src="/email-icon.svg" width={30} alt=""/>
@@ -34,29 +29,9 @@ function Login() {
             <div className="submit-container">
                 <button type="button" onClick={handleLogin}>Sign In</button>
             </div>
-=======
-                <form className="inputs">
-                    <div className="input">
-                        <img src="/email-icon.svg" width={30} alt=""/>
-                        <input type="email" placeholder="Email" value={email} onChange={(e) => setEmail(e.target.value)} />
-                    </div>
-                    <div className="input">
-                        <img src="/passwor-icon.svg" width={30} alt=""/>
-                        <input type="password" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
-                    </div>
-                </form>
-                <div className="submit-container">
-                    <button type="button" onClick={handleLogin}>Sign In</button>
-                </div>
->>>>>>> origin/master
 
         </div>
     );
 }
 
-<<<<<<< HEAD
 export default Login;
-=======
-export default Login;
-
->>>>>>> origin/master
